54ed02da3a0b5169bedb242d876ff98e2804824a
Colin Brownlie <colinmbrownlie@gmail.com>
2016-09-21 09:35:17 -0700

    added endpoints for CRUD methods

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const dbMethods = require('../database/databaseMethods');
 const webpack = require('webpack');
+const bodyParser = require('body-parser');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack.config');
 
 app.use(express.static(path.join(__dirname, './../')));
-
+app.use(bodyParser, {extendedurl: true})
 
 
 app.listen(3000, () => {
@@ -17,9 +18,21 @@ app.post('/', (req, res) => {
   console.log('its working yandri!');
 });
 
+app.post('/createPoll', (req, res, next) => {
+  dbMethods.createPollInstance(req.body, next)
+});
 
+app.get('/getPoll', (req, res, next) => {
+  dbMethods.returnPollInstance(req.body, next)
+});
 
+app.post('/updatePoll', (req, res, next) => {
+  dbMethods.updatePollInstance(req.body, next)
+});
 
+app.post('/deletePoll', (req, res, next) => {
+  dbMethods.deletePollInstance(req.body, next)
+});
 //this starts the webpack-dev-server instead of having to start it manually.
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
