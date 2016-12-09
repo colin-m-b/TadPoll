870cc3d11cece8c5e67d433a8b816a60a85bcf3b
Colin Brownlie <colinmbrownlie@gmail.com>
2016-12-09 13:49:11 -0800

    more work on editing poll

--- components/updatePoll.js
+++ components/updatePoll.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { link, browserHistory } from 'react-router'
+import Answer from './createAnswers'
 
 export default class UpdatePoll extends Component {
 
@@ -16,9 +17,50 @@ export default class UpdatePoll extends Component {
 
     render() {
         let questions = []
+        for (let i = 1; i < 11; i++) {
+            let quesVal = ""
+            if (this.props.getAppState.questions[i]) {
+                let item = this.props.getAppState.questions[i]
+                quesVal = item.question
+                let answers = []
+                for (let j = 1; j < 5; j++) {
+                    let answerVal = ""
+                    if (item.answers[j]) answerVal = item.answers[j].answer 
+                    answers.push(
+                    <span>
+                        <label>Answer {j}</label>
+                        <input defaultValue={answerVal} key={j} className="answer-input"/>
+                    </span>
+                    )
+                }
 
-        this.props.getAppState.questions.forEach(function(item) {
-            questions.push(item)
-        })
+                questions.push(
+                    <div className="edit-question-div" key={i}>
+                        <p className="ques-label">Question {i}</p>
+                        <form>
+                            <input 
+                            defaultValue={item.question}
+                            id="question" 
+                            size="100"
+                            placeholder="question" 
+                            maxLength="200" />
+                            <title>Aswers</title>
+                            {answers}
+                        </form>
+                    </div>
+                )
+            }
+        }
+        return (
+            <div>
+                <h1>Poll {this.props.getAppState.pollTitle}</h1>
+                <h3>Edit questions below</h3>
+                {questions}
+                Click to save poll 
+                <button type="button" onClick={this.updatePoll}>Save poll</button>
+            </div>
+        )
     }
+        
+    
 }
