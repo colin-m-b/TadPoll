f95761970c3e677907fe3b334f0df2f363335875
Colin Brownlie <colinmbrownlie@gmail.com>
2017-02-02 17:56:49 -0800

    added user functionality

---
+++ components/answerPoll.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link, browserHistory } from 'react-router'
+import $ from 'jQuery'
+
+export default class AnswerPoll extends Component {
+
+    constructor(props) {
+        super(props)
+        this.buildAnswers = this.buildAnswers.bind(this)
+    }
+
+
+    buildAnswers() {
+        const answerArray = []
+        console.log(this.props.getAppState.userQuestions)
+        this.props.getAppState.userQuestions.answers.forEach()
+    }
+}
